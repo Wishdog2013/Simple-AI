d54451717405da936d2bc2c79d2c857094eb2c1d
Wish <wishdog2013@hotmail.com>
2023-06-04 13:47:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Define the chatbot responses
 const chatbotResponses = {
-  "hi": "Hello!",
+  "^hi$": "Hello!",
   "hello": "Hello! How can I assist you today?",
   "how are you": "I'm an AI, so I don't have feelings, but thank you for asking!",
   "what is your name": "I'm Simple Bot, a basic AI chatbot.",
@@ -11,23 +11,32 @@ const chatbotResponses = {
 function processInput() {
   const userInput = document.getElementById("userinput").value.toLowerCase();
   const chatlog = document.getElementById("chatlog");
-  const chatbotResponse = chatbotResponses[userInput] || chatbotResponses["default"];
-  
+  let chatbotResponse = chatbotResponses["default"];
+
+  // Check for matching user input in chatbotResponses
+  for (const pattern in chatbotResponses) {
+    const regex = new RegExp(pattern, "i");
+    if (regex.test(userInput)) {
+      chatbotResponse = chatbotResponses[pattern];
+      break;
+    }
+  }
+
   // Display user input and chatbot response in the chatlog
   const userMessage = document.createElement("p");
   userMessage.innerText = "You: " + userInput;
   chatlog.appendChild(userMessage);
-  
+
   const chatbotMessage = document.createElement("p");
   chatbotMessage.innerText = "Chatbot: " + chatbotResponse;
   chatlog.appendChild(chatbotMessage);
-  
+
   // Clear the user input field
   document.getElementById("userinput").value = "";
 }
 
 // Process user input when Enter key is pressed
-document.getElementById("userinput").addEventListener("keydown", function(event) {
+document.getElementById("userinput").addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     processInput();
   }
